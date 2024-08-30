d35ae2396edb7ebafd5524478745d2314bd7a2ef
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2024-08-30 17:23:23 +0000

    Added to java blackjack

--- Java/BlackJackGUI.java
+++ Java/BlackJackGUI.java
@@ -1,7 +1,10 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class BlackJackGUI {
     public static void createFrame() {
-        JFrame("Simple GUI");
+        JFrame frame = new JFrame("Window");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JLabel textLabel = new JLabel("I'm a label in the window",SwingConstants.CENTER);
         textLabel.setPreferredSize(new Dimension(300, 100));
