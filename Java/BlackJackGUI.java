6bfdb6cbc1866f09dd81a2efeea3d740470a1b87
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2024-08-30 17:28:51 +0000

    added stuff

--- Java/BlackJackGUI.java
+++ Java/BlackJackGUI.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -7,11 +8,20 @@ public class BlackJackGUI {
         JFrame frame = new JFrame("Window");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JLabel textLabel = new JLabel("I'm a label in the window",SwingConstants.CENTER);
-        textLabel.setPreferredSize(new Dimension(300, 100));
+        textLabel.setPreferredSize(new Dimension(300, 100)));
         frame.getContentPane().add(textLabel, BorderLayout.CENTER);
         frame.pack();
         frame.setVisible(true);
     }
+
+    public static void createDeck() {
+
+    }
+
+    public static void gameLogic() {
+        
+    }
+
     public static void main(String[] args) {
 
     }
