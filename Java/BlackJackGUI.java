7f51fe38aa699efe9812573bf9df5b26ac19ef79
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2024-09-01 20:22:59 -0400

    Added stuff and added code hygine (no stink)

--- Java/BlackJackGUI.java
+++ Java/BlackJackGUI.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.text.DecimalFormat;
 
-public class BlackJackGUI extends JFrame {
+public class BlackJackGUI {
 
     static JFrame frame;
     static int moneyRaw = 100;
@@ -10,6 +10,8 @@ public class BlackJackGUI extends JFrame {
     static int[][] deck = new int[4][13];
     static DecimalFormat currency = new DecimalFormat("$,##0");
     static Image img;
+    static JLabel moneyTxt = new JLabel();
+    static JPanel monPanel = new JPanel();
 
     public static void main(String[] args) {
         createFrame();
@@ -18,29 +20,52 @@ public class BlackJackGUI extends JFrame {
         showCard("Back", 180, 30);
         showCard(drawCard(), 50, 350);
         showCard(drawCard(), 180, 350);
-        money(100);
-        money(1000000);
-
-        //System.out.println(drawCard());
+        money(0);
+        
     }
 
     public static void createFrame() {
+        //creating the window itself
         frame = new JFrame();
-        frame.setTitle("Blackjack");
-        frame.setLayout(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(0, 0, 440, 700); 
-        frame.setLocationRelativeTo(null);
-        frame.getContentPane().setBackground(new java.awt.Color(35, 31, 32));
-        frame.setVisible(true);
+        frame.setTitle("Blackjack"); //title
+        frame.setLayout(null); //no layout, using the x y plane
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //makes sure that the window will close when x is pressed
+        frame.setBounds(0, 0, 440, 700);  //set the size of the window
+        frame.setLocationRelativeTo(null); // idk
+        frame.getContentPane().setBackground(new java.awt.Color(35, 31, 32)); //set background color
+        frame.setVisible(true); //makes window visible 
+
         //background color rgb(35,31,32) or 231F20
+
+        /*    creating hit and stand buttons    */
+        //creating objects
+        JButton hit = new JButton("Hit");
+        JButton stand = new JButton("Stand");
+        JPanel hitPan = new JPanel();
+        JPanel standPan = new JPanel();
+        //setting hit panel size, location, and background color
+        hitPan.add(hit);
+        hitPan.setLocation(80,560);
+        hitPan.setSize(70,35);
+        hitPan.setBackground(new java.awt.Color(35, 31, 32));
+        //setting stand panel size, location, and background color
+        standPan.add(stand);
+        standPan.setLocation(210,560);
+        standPan.setSize(70,35);
+        standPan.setBackground(new java.awt.Color(35, 31, 32));
+        //adding the panels to the frame
+        frame.add(hitPan);
+        frame.add(standPan);
+
     }
 
     public static void showCard(String card, int x, int y) {
+        //formats the card so that it can be found with resources 
         card += ".png";
         card = "cards/" + card;
 
-        Container c = frame.getContentPane();
+        //adding the image to the frame
+        Container c = frame.getContentPane(); //idk, frame.getContentPlane wont work without a container so here
         JLabel cardImage = new JLabel();
         cardImage.setIcon(new ImageIcon(card));
         Dimension size = cardImage.getPreferredSize();
@@ -62,38 +87,40 @@ public class BlackJackGUI extends JFrame {
     public static String drawCard() {
         String cardDrawn;
 
+        //first randomly drawn card
         int ranSuit = (int)(Math.random() * 4);
         int ranFace = (int)(Math.random()* 13);
 
-
+        //makes sure that the card has not already been drawn
         while((deck[ranSuit][ranFace] > 100)) {
             ranSuit = (int)(Math.random() * 4);
             ranFace = (int)(Math.random()* 13);
         }
         
+        //shows that the card has been played
         deck[ranSuit][ranFace] = 100;
+        //makes the card name so that it can be displayed with the card images
         cardDrawn = ranSuit + "_" + ranFace;
 
         return cardDrawn;
     }
 
     public static void gameLogic(int[][] Cards) {
-        Cards = new int[2][23];
-
+        Cards = new int[2][23]; //played cards
     }
 
     public static void money(int change) {
-        moneyRaw += change;
-        moneyFormat = currency.format(moneyRaw);
-
-        JLabel money = new JLabel(moneyFormat);
-        JPanel panel = new JPanel();
-        panel.add(money);
-        frame.add(panel);
-        panel.setLocation(0,0);
-        panel.setSize(440,50);
-        panel.setBackground(new java.awt.Color(35, 31, 32));
-        panel.setForeground(new java.awt.Color(255,255,255));
+        moneyRaw += change; //cahnges the money
+        moneyFormat = currency.format(moneyRaw); //formats the money
+        
+        moneyTxt.setText(moneyFormat); 
+        
+        monPanel.add(moneyTxt);
+        frame.add(monPanel);
+        monPanel.setLocation(0,0);
+        monPanel.setSize(440,50);
+        monPanel.setBackground(new java.awt.Color(35, 31, 32)); // panel color
+        moneyTxt.setForeground(new java.awt.Color(255,255,255)); //text color 
         SwingUtilities.updateComponentTreeUI(frame); //Refresh the frame
         //System.out.println(moneyFormat);
     }
