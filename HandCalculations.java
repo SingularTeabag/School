25d99329042c395be0f764f8b680e49bfd8224ec
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2024-09-10 12:23:43 -0400

    Add files via upload

---
+++ HandCalculations.java
@@ -0,0 +1,43 @@
+public class HandCalculations {
+    public static void main(String[] args) {
+        double w, y, s;
+        int a, b, c;
+
+        w = 12.0;
+        y = 3.5;
+        a = 3;
+        b = 2;
+        c = 7;
+
+        System.out.println("a. " + (a + b * c) + "\n");
+        System.out.println("b. " + (a - b - c) + "\n");
+        System.out.println("c. " + (a / b) + "\n");
+        System.out.println("d. " + (b / a) + "\n");
+        System.out.println("e. " + (a - b / c) + "\n");
+        System.out.println("f. " + (w / a) + "\n");
+        System.out.println("g. " + (a / w) + "\n");
+        System.out.println("h. " + (a + w / b) + "\n");
+        System.out.println("i. " + ((a + w) / b) + "\n");
+        System.out.println("j. " + (c / y * a) + "\n");
+        System.out.println("k. " + (b % a) + "\n");
+        System.out.println("l. " + (b % a + 3) + "\n");
+        System.out.println("m. " + ((a + 3) % b) + "\n");
+        
+        a = a * 2;
+        c = c / 2;
+        
+        System.out.println("a. " + (a + b * c) + "\n");
+        System.out.println("b. " + (a - b - c) + "\n");
+        System.out.println("c. " + (a / b) + "\n");
+        System.out.println("d. " + (b / a) + "\n");
+        System.out.println("e. " + (a - b / c) + "\n");
+        System.out.println("f. " + (w / a) + "\n");
+        System.out.println("g. " + (a / w) + "\n");
+        System.out.println("h. " + (a + w / b) + "\n");
+        System.out.println("i. " + ((a + w) / b) + "\n");
+        System.out.println("j. " + (c / y * a) + "\n");
+        System.out.println("k. " + (b % a) + "\n");
+        System.out.println("l. " + (b % a + 3) + "\n");
+        System.out.println("m. " + ((a + 3) % b) + "\n");
+    }
+}
