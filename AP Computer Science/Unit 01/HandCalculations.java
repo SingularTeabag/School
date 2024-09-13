cddc8d37cc752a8f4ff49c95d10715a1dcf8f671
SingularTeabag <awesomekidphill@yahoo.com>
2024-09-13 13:51:28 -0400

    Update HandCalculations.java

--- AP Computer Science/Unit 01/HandCalculations.java
+++ AP Computer Science/Unit 01/HandCalculations.java
@@ -9,39 +9,39 @@ public class HandCalculations {
         b = 2;
         c = 7;
 
-        System.out.println("Task 1:");
-        System.out.println("a. " + (a + b * c));
-        System.out.println("b. " + (a - b - c));
-        System.out.println("c. " + (a / b));
-        System.out.println("d. " + (b / a));
-        System.out.println("e. " + (a - b / c));
-        System.out.println("f. " + (w / a));
-        System.out.println("g. " + (a / w));
-        System.out.println("h. " + (a + w / b));
-        System.out.println("i. " + ((a + w) / b));
-        System.out.println("j. " + (c / y * a));
-        System.out.println("k. " + (b % a));
-        System.out.println("l. " + (b % a + 3));
-        System.out.println("m. " + ((a + 3) % b));
+        System.out.println("\nTask 1:");
+        System.out.println("a. " + a + " + " + b + " / " + c + " = " + (a + b * c));
+        System.out.println("b. " + a + " - " + b + " - " + c + " = " + (a - b - c));
+        System.out.println("c. " + a + " / " + b + " = " + (a / b));
+        System.out.println("d. " + b + " / " + a + " = " + (b / a));
+        System.out.println("e. " + a + " - " + b + " / " + c + " = " + (a - b / c));
+        System.out.println("f. " + w + " / " + a + " = " + (w / a));
+        System.out.println("g. " + a + " / " + w + " = " + (a / w));
+        System.out.println("h. " + a + " + " + w + " / " + b + " = " + (a + w / b));
+        System.out.println("i. " + "(" + a + " + " + b + ") / " + b + " = " + ((a + w) / b));
+        System.out.println("j. " + c + " / " + y + " * " + a + " = " + (c / y * a));
+        System.out.println("k. " + b + " % " + a + " = " + (b % a));
+        System.out.println("l. " + b + " % " + a + " + 3 = " +(b % a + 3));
+        System.out.println("m. " + "(" + a + " + 3) % " + b + " = " + ((a + 3) % b));
         
         a *= 2;
         c /= 2;
         
         System.out.println("\nTask 3:");
 
-        System.out.println("a. " + (a + b * c));
-        System.out.println("b. " + (a - b - c));
-        System.out.println("c. " + (a / b));
-        System.out.println("d. " + (b / a));
-        System.out.println("e. " + (a - b / c));
-        System.out.println("f. " + (w / a));
-        System.out.println("g. " + (a / w));
-        System.out.println("h. " + (a + w / b));
-        System.out.println("i. " + ((a + w) / b));
-        System.out.println("j. " + (c / y * a));
-        System.out.println("k. " + (b % a));
-        System.out.println("l. " + (b % a + 3));
-        System.out.println("m. " + ((a + 3) % b));
+        System.out.println("a. " + a + " + " + b + " / " + c + " = " + (a + b * c));
+        System.out.println("b. " + a + " - " + b + " - " + c + " = " + (a - b - c));
+        System.out.println("c. " + a + " / " + b + " = " + (a / b));
+        System.out.println("d. " + b + " / " + a + " = " + (b / a));
+        System.out.println("e. " + a + " - " + b + " / " + c + " = " + (a - b / c));
+        System.out.println("f. " + w + " / " + a + " = " + (w / a));
+        System.out.println("g. " + a + " / " + w + " = " + (a / w));
+        System.out.println("h. " + a + " + " + w + " / " + b + " = " + (a + w / b));
+        System.out.println("i. " + "(" + a + " + " + b + ") / " + b + " = " + ((a + w) / b));
+        System.out.println("j. " + c + " / " + y + " * " + a + " = " + (c / y * a));
+        System.out.println("k. " + b + " % " + a + " = " + (b % a));
+        System.out.println("l. " + b + " % " + a + " + 3 = " +(b % a + 3));
+        System.out.println("m. " + "(" + a + " + 3) % " + b + " = " + ((a + 3) % b));
 
         //Output \/
         /*
