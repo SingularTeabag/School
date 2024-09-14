18f968bef56fe7192f4915f86ac5c7949b43dfa8
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2024-09-14 19:48:16 -0400

    Add files via upload

---
+++ AP Computer Science/Unit 01/ProblemSolvingWithPrimitives2.java
@@ -0,0 +1,37 @@
+public class ProblemSolvingWithPrimitives2 {
+    public static void main(String[] args) {
+
+        /*Question 16: An integer constant 'dayOfWeek1' has a value from 0 to 6 and represents the 
+                       day of the week for January 1st (0=Sunday, 1=Monday, etc.). A variable
+                       'day' has a value from 1 to 31 and represents a day in January. Write an 
+                       expression that calculates the day of the week for any given value of 'day'.
+                       For example, if 'dayOfWeek1 = 0'(January 1st is a Sunday) and 'day = 13'.
+                       (January 13th), then 'dayOfWeek', the day of the week for January 13th,
+                       should get a value of 5 (Friday).*/
+
+        /*Question 18: Rewrite the Gas Mileage to prompt the user to enter the gas mileage and the 
+                       number of miles and display the amount of gas spnt in htat trip.*/
+
+        /*Question 19: Write a short class 'LunchTime' with no fields, no constructors, and just one 
+                       static method 'minutesUntilLunch'. The method should take two 'int' 
+                       parameters, current hours and minutes (before 1 p.m.). and return (not print!)
+                       the number of minutes left until lunch, scheduled for 1 p.m. Add a 'main'
+                       method that prompts the user for the current time (in the format hh:mm),
+                       calls'minutesUntilLaunch', and prints the value returned by the call. Make
+                       sure your program displays the correct result when the user enters 9:00,
+                       10:10, 12:00, 12:50.*/
+
+        /*Question 20: 
+        */   
+        /*Question 21:
+        */
+        /*Question 22:
+        */
+        /*Question 23:
+        */
+        /*Question 24:
+        */
+        /*Question 25:
+        */
+    }
+}
