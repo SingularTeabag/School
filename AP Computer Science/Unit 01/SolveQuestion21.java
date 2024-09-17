7bcfc09fbc735c05b7f09fe5a6fbfae6d97cac74
SingularTeabag <awesomekidphill@yahoo.com>
2024-09-17 14:15:07 -0400

    School

---
+++ AP Computer Science/Unit 01/SolveQuestion21.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SolveQuestion21 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter height (inches): ");
+        int height = input.nextInt();
+        
+        System.out.print("Enter weight (lbs): ");
+        int weight = input.nextInt();
+        
+        double bmi = calculateBMI(height, weight);
+        System.out.println("BMI: " + bmi);
+    }
+    
+    public static double calculateBMI(int height, int weight)
+    {
+        return weight * 0.454 / Math.pow(height * 0.0254,2);
+    }
+}
