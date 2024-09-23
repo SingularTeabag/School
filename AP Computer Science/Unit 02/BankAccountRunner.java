91ad42c825a3d7d09e782c2dccd258e62c55354d
SingularTeabag <awesomekidphill@yahoo.com>
2024-09-23 14:20:51 -0400

    school

---
+++ AP Computer Science/Unit 02/BankAccountRunner.java
@@ -0,0 +1,45 @@
+/*
+ * Author:  <you>
+ * Date:    9/24
+ * Purpose: Create and manipulate BankAccount objects
+ */
+
+ public class BankAccountRunner
+ {
+     public static void main(String[] args)
+     {
+         
+        // Create the following BankAcount objects:
+        //   - collegeFund, with ID 82890 and a starting balance 
+        //     of $47,500
+        //   - retirementSavings, with ID 99999 and a starting 
+        //     balance of $25,000
+ 
+         
+        // Complete the following 2 S.o.p statements to display account 
+        // details
+         
+        System.out.println("_____________");
+        System.out.println("_____________");
+         
+        // Deposit $597 to collegeFund
+         
+        // Deposit $1,000 to retirementSaving
+         
+        // Withdraw $159.95 from collegeFund
+         
+        // Deposit $159.36 to collegeFund
+         
+        // Deposit $1,500 to retirementSavings  
+         
+        // Withdraw $250 from retirementSavings  
+         
+        // Copy/paste your S.o.p statements from above to display the current 
+        // balance in each account.  TRACE the transactions for each account
+        // and compare to your output
+        
+        System.out.println();
+         
+ 
+     }    
+ }
