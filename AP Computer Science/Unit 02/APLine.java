ce129d943d084dabc5a9e6a32992298a7c63a813
SingularTeabag <129084658+SingularTeabag@users.noreply.github.com>
2024-10-04 14:31:19 -0700

    Add files via upload

--- AP Computer Science/Unit 02/APLine.java
+++ AP Computer Science/Unit 02/APLine.java
@@ -1,37 +1,68 @@
 public class APLine {
+    //declare object variables
     private int a;
     private int b;
     private int c;
 
+    //no args constructor
     public APLine() {
         a = 1;
         b = 1; 
         c = 0;
     }
 
+    //APLine constructor
     public APLine(int userA, int userB, int userC) {
         a = userA;
         b = userB;
         c = userC;
     }
 
+    //returns the slope of the line
     public double getSlope() {
         return (double)-a/b;
     }
 
+    //returns true or false if the point is on the line
     public boolean isOnLine(int userX, int userY) {
         return (a * userX + b * userY + c) == 0;
     }
 
+    //turns the variables of the object into an equation
     public String toString() {
-        String output = a + "x + " + b + "y ";
+        String output = a + "x " ;
+        if (b < 0) {
+            output += "- " + Math.abs(b) + "y ";
+        }
+        else 
+            output += "+ " + b + "y ";
+
         if (c < 0) {
             output += "- " + Math.abs(c) + " = 0";
-            return output;
-        } 
-        else {
-            output += "+ " + c + " = 0";
-            return output;
         }
+        else 
+            output += "+ " + c + " = 0";
+        return output;
+    }
+
+    //returns variable "a"
+    public int getA() {
+        return a;
+    }
+
+    //returns variable "b"
+    public int getB() {
+        return b;
+    }
+
+    //returns variable "c"
+    public int getC() {
+        return c;
+    }
+    
+    //returns the y value given the x value of the equation
+    public double findY(int x) {
+        double output = (int)(a * x + c) / (-1 * b);
+        return output;
     }
 }
