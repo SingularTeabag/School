f887bca1c7edb37f9484a2ea099d3fd6999c68cc
SingularTeabag <awesomekidphill@yahoo.com>
2024-10-07 14:21:05 -0400

    school

---
+++ AP Computer Science/Unit 02/StringInvestigation.java
@@ -0,0 +1,12 @@
+public class StringInvestigation {
+    public static void main(String[] args) {
+        String str1;
+        str1 = "AP Computer Science";
+        String str2 = "cats";
+        String str3 = new String("dogs");
+
+        System.out.println("Oh, no, it's, raining " + str2 + " and " + str3 + "!");
+
+        System.out.println(str1);
+    }
+}
